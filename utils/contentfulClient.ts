f9c5fdb48de06e8c5de7996fdc08f07789025cf3
Peterr181 <piotrzerdzinski089@gmail.com>
2024-12-01 12:17:42 +0100

    fix undefined err

--- utils/contentfulClient.ts
+++ utils/contentfulClient.ts
@@ -1,9 +1,9 @@
 import { createClient } from "contentful";
 
 const client = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  environment: process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT, // defaults to 'master' if not set
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "",
+  environment: process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT || "master",
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "",
 });
 
 export default client;
